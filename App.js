4486dc98b8396876de43ebf050cf4f797b1a1749
NOWAR <nowar@MacBookPro.lan>
2026-09-18 00:22:16 +0200

    Brighten terracotta app background

--- App.js
+++ App.js
@@ -657,7 +657,7 @@ export default function App() {
 }
 
 const APP_PALETTE = {
-  "#FFF8F2": "#C97859",
+  "#FFF8F2": "#D86843",
   "#FFFDFC": "#FFFCF7",
   "#F8EAE0": "#EEE2D6",
   "#FFF0E9": "#F3DDD3",
